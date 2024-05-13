3c6888882f970abe11cc05a4e148520d2636c042
Mohammed <s202016140@kfupm.edu.sa>
2024-05-13 22:52:25 +0300

    database files

---
+++ Fly_Aslam/Dump20240513/mainschemea_flight.sql
@@ -0,0 +1,58 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mainschemea
+-- ------------------------------------------------------
+-- Server version	5.7.44-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `flight`
+--
+
+DROP TABLE IF EXISTS `flight`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flight` (
+  `FlightID` char(8) NOT NULL DEFAULT '00000000',
+  `Source` varchar(45) DEFAULT NULL,
+  `Destination` varchar(45) DEFAULT NULL,
+  `ArriveTime` varchar(20) DEFAULT NULL,
+  `DepartureTime` varchar(20) DEFAULT NULL,
+  `Date` varchar(20) DEFAULT NULL,
+  `aicraftID` varchar(8) DEFAULT NULL,
+  PRIMARY KEY (`FlightID`),
+  KEY `aircraftID_idx` (`aicraftID`),
+  CONSTRAINT `aircraftID` FOREIGN KEY (`aicraftID`) REFERENCES `aircrafts` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flight`
+--
+
+LOCK TABLES `flight` WRITE;
+/*!40000 ALTER TABLE `flight` DISABLE KEYS */;
+INSERT INTO `flight` VALUES ('11111111','MED','DMM','10:00AM','8:00PM','12/9/2024','11111111'),('11111112','DMM','MED','10:00PM','8:00AM','12/9/2024',NULL);
+/*!40000 ALTER TABLE `flight` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-05-13 22:46:01
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const http = require('http2');
+
 /*
 
 rootPassword123
 
 */
-var con = mysql.createConnection({
+const con = mysql.createConnection({
     host: "localhost",
     user: "yourusername",
     password: "YourPassword123"
@@ -17,6 +17,8 @@ con.connect(function (err) {
     if (err) throw err;
     console.log("Connected!");
 });
+
+
 
 
 app.listen(3000);
